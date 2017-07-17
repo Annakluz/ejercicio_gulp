8b1933e30ba6b84f1f81f5106acf486b07add9f3
Anna karina Luz <anna.luz.duran@gmail.com>
2017-07-17 16:38:12 -0500

    listo todo

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,14 @@ var browserSync = require("browser-sync").create();
 
 var rutas = {
     rutaJS: "./src/assets/js/*.js",
-    rutaSCSS: "./src/assets/scss/*.scss"
+    rutaSCSS: "./src/assets/scss/*.scss",
+    rutaHTML:"src/*.html",
 }
+
+gulp.task("prepararHTML", function(){
+    gulp.src(rutas.rutaHTML)
+    .pipe(gulp.dest("public/"))
+});
 
 gulp.task("prepararJS", function(){
     gulp.src(rutas.rutaJS)
@@ -30,10 +36,19 @@ gulp.task("watchChangesCSS", function(){
             baseDir:"./public"
         }
     });
-    gulp.watch(rutas.rutaSCSS,["sass-watch"])
+    gulp.watch(rutas.rutaSCSS,["sass-watch"]);
+    gulp.watch(rutas.rutaJS, ["js-watch"]);
+    gulp.watch(rutas.rutaHTML, ["html-watch"]);
 });
 
 gulp.task("sass-watch", ["prepararCSS"], function(){
     browserSync.reload();
 });
 
+gulp.task("js-watch", ["prepararJS"], function(){
+    browserSync.reload();
+});
+
+gulp.task("html-watch", ["prepararHTML"], function(){
+    browserSync.reload();
+});
